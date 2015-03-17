00898f210a12b0a51b9a71ef6261a3bcee6b864b
ChenJianzhao <j.623487211@163.com>
2015-03-18 00:30:57 +0800

    添加 MainController

---
+++ src/main/java/org/demo/controller/MainController.java
@@ -0,0 +1,19 @@
+package org.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by jzchen on 2015/3/18 0018.
+ */
+@Controller
+@RequestMapping("/web")
+public class MainController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String abc() {
+        System.out.println("jin ru webapp");
+        return "web/webapp";
+    }
+}
