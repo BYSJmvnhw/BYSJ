7a5d44c08f1805bbd63f51be286bbcc8e0dd3c93
zquancai <zquancai@126.com>
2015-03-22 18:05:41 +0800

    优化模块间的通信，增强了模块的可扩展性。approuter为主控制器

--- src/main/webapp/WEB-INF/jsp/web/webapp.jsp
+++ src/main/webapp/WEB-INF/jsp/web/webapp.jsp
@@ -5,12 +5,14 @@
     <meta charset="UTF-8">
     <title>个人信息</title>
     <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/resources/skin/css/login.css">
-    <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/resources/skin/css/webapp.css">
+    <%--<link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/resources/skin/css/webapp.css">--%>
     <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/resources/skin/css/transition.css">
     <script type="text/javascript" src="${pageContext.request.contextPath}/resources/lib/sea-2.3.0.js"  id="seajsnode"></script>
 </head>
 <body>
-    <section id="login">
+    <section id="login" role="login"></section>
+    <section id="main" class="main" role="main"></section>
+    <script type="text/html" id="login-html">
         <section id="login-p" class="login-p t-login t-login-open" role="login-p">
             <section class="lg-header">
                 <div class="lg-bg"></div>
@@ -37,21 +39,36 @@
                     </p>
                 </div>
             </section>
+            <div id="lg-shade" class="lg-shade">
+                <div class="lg-shade-tip">
+                    <p><strong>正在登陆...</strong></p>
+                    <span class="t-load"></span>
+                </div>
+            </div>
         </section>
-    </section>
-    <section id="main" class="main" role="main"></section>
-    <script type="text/html" id="login-html"></script>
+    </script>
     <script type="text/html" id="main-html">
         <nav>
             <div class="nav-wrap" role="navigation">
-                <ul class="nav-main" role="menubar">
-                    <li class="logo" role="logo">
-                        <a href="#"><span></span></a>
+                <ul id="nav-main" class="nav-main" role="menubar">
+                    <li class="logo" role="logo"><span></span></li>
+                    <li class="active">首页</li>
+                    <li>
+                        个人中心
+                        <ul data-type="man" class="type1 nav-main-child t-top">
+                            <li data-bar="info">个人信息</li>
+                            <li data-bar="changepw">密码修改</li>
+                            <li data-bar="setmail">设置邮箱</li>
+                        </ul>
                     </li>
-                    <li class="active"><a href="#">首页</a></li>
-                    <li><a href="#">个人中心</a></li>
-                    <li><a href="#">作业管理</a></li>
-                    <li><a href="#">帮助</a></li>
+                    <li>
+                        作业管理
+                        <ul data-type="hkmanage" class="type2 nav-main-child t-top">
+                            <li data-bar="hkinfo">作业信息</li>
+                            <li data-bar="hkdynamic">作业动态</li>
+                        </ul>
+                    </li>
+                    <li>帮助</li>
                 </ul>
                 <div class="welcome">
                     <span>您好，陈培峰</span>
@@ -75,7 +92,7 @@
         </header>
         <section class="main-content">
             <div id="left-nav" class="left-nav" role="navigation">
-                <div class="l-menu" data-type="man"><strong><span class="man"></span>个人中心<span class="bn-slide t-rotate t-rotate-open"></span></strong></div>
+                <div class="l-menu" data-type="man"><strong><span class="man"></span>个人中心<span class="bn-slide t-rotate t-rotate-close"></span></strong></div>
                 <ul class="t-slide t-close">
                     <li data-bar="info">个人信息</li>
                     <li data-bar="changepw">密码修改</li>
@@ -83,42 +100,12 @@
                 </ul>
                 <div class="l-menu" data-type="hkmanage"><strong><span class="work"></span>作业管理<span class="bn-slide t-rotate t-rotate-close"></span></strong></div>
                 <ul class="t-slide t-close">
-                    <li data-bar="hklist">作业列表</li>
-                    <li>密码修改</li>
-                    <li>设置邮箱</li>
+                    <li data-bar="hkinfo">作业信息</li>
+                    <li data-bar="hkdynamic">作业动态</li>
                 </ul>
             </div>
             <div id="content" class="content">
                 <!--主要信息-->
-                <div class="personal-info">
-                    <div class="p-name">
-                        <label><strong>姓名</strong></label>
-                        <label>{{name}}</label>
-                    </div>
-                    <div class="p-name">
-                        <label><strong>性别</strong></label>
-                        <label>{{sex}}</label>
-                    </div>
-                    <div class="p-name">
-                        <label><strong>学号</strong></label>
-                        <label>{{studentNo}}</label>
-                    </div>
-                    <div class="p-name">
-                        <label><strong>校区学院</strong></label>
-                        <label>{{hwCampus}}-{{hwCollege}}</label>
-                    </div>
-                    <div class="p-name">
-                        <label><strong>年级班级</strong></label>
-                        <label>{{grade}}级{{class_}}班</label>
-                    </div>
-                    <div class="p-name">
-                        <label><strong>专业</strong></label>
-                        <label>{{hwMajor}}</label>
-                    </div>
-                </div>
-                <div class="personal-img">
-                    <img src="${pageContext.request.contextPath}/resources/skin/images/man.jpg">
-                </div>
             </div>
         </section>
         <footer>
@@ -138,24 +125,45 @@
         <div class="personal-info">
             <div class="p-name">
                 <label><strong>姓名</strong></label>
-                <input type="text" value="{{name}}" readonly/>
+                <label>{{name}}</label>
             </div>
             <div class="p-name">
-                <label><strong>学号</strong></label>
-                <input type="text" value="{{sn}}" readonly/>
+                <label><strong>性别</strong></label>
+                <label>{{sex}}</label>
             </div>
             <div class="p-name">
-                <label><strong>学院</strong></label>
-                <input type="text" value="{{college}}" readonly/>
+                {{if userType == 'STUDENT'}}
+                    <label><strong>学号</strong></label>
+                    <label>{{studentNo}}</label>
+                {{else if userType == 'TEACHER'}}
+                    <label><strong>教师号</strong></label>
+                    <label>{{teacherNo}}</label>
+                {{/if}}
+            </div>
+            <div class="p-name">
+                <label><strong>校区学院</strong></label>
+                <label>{{hwCampus}}-{{hwCollege}}</label>
+            </div>
+            <div class="p-name">
+                <label><strong>年级班级</strong></label>
+                <label>{{grade}}级{{class_}}班</label>
             </div>
             <div class="p-name">
                 <label><strong>专业</strong></label>
-                <input type="text" value="{{major}}" readonly/>
+                <label>{{hwMajor}}</label>
             </div>
         </div>
         <div class="personal-img">
             <img src="${pageContext.request.contextPath}/resources/skin/images/man.jpg">
         </div>
+    </script>
+    <script id="test" type="text/html">
+        <h1>{{title}}</h1>
+        <ul>
+            {{each list as value i}}
+            <li>索引 {{i + 1}} ：{{value}}</li>
+            {{/each}}
+        </ul>
     </script>
     <!--<script type="text/javascript" src="script/webapp.js"></script>-->
     <!--<script type="text/javascript" src="script/login.js"></script>-->
@@ -172,6 +180,7 @@
                 'login': 'script/login.js',
                 'jquery': 'jquery-1.11.2.min.js',
                 'jquery-plugin': 'jquery-plugin.js',
+                'template': 'template.js',
                 'backbone': 'backbone-min.js',
                 'underscore': 'underscore-min.js',
                 'webapp': 'script/webapp.js?v=201503181523',
--- src/main/webapp/resources/script/webapp.js
+++ src/main/webapp/resources/script/webapp.js
@@ -3,13 +3,20 @@
  */
 
 
-define(['jquery-plugin', 'underscore', 'backbone'], function(require, exports, module) {
+define(['jquery-plugin', 'underscore', 'backbone', 'template'], function(require, exports, module) {
 
     var $ = require('jquery-plugin').$;
     var _ = require('underscore');
     var Backbone = require('backbone');
+    var tmpl = require('template');
     Backbone.$ = $; // 使用cmd时需要手动引入$
-
+    var servicepath = 'http://localhost:8080/mvnhk/';
+//    var data = {
+//        title: '标签',
+//        list: ['文艺', '博客', '摄影', '电影', '民谣', '旅行', '吉他']
+//    };
+//    var html = tmpl('test', data);
+//    console.log(html);
     var HomeWork = Backbone.Model.extend({
         initialize: function () {
             console.log('model');
@@ -42,7 +49,7 @@ define(['jquery-plugin', 'underscore', 'backbone'], function(require, exports, m
         },
         render: function () {
             console.log('render');
-            var ele = _.template($('#man-info').html())(this.model.toJSON());
+            var ele = tmpl('man-info', this.model.toJSON());
             this.$el.html(ele);
             return this;
         }
@@ -52,6 +59,7 @@ define(['jquery-plugin', 'underscore', 'backbone'], function(require, exports, m
         el: $('#main'),
         events: {
             'click': 'docEvent', // 隐藏一些弹出框
+            'click #nav-main>li>ul>li': 'showStateData', // 顶栏菜单
             'click #left-nav>.l-menu': 'togSlide', // 左侧菜单栏选项事件
             'click #left-nav>ul>li': 'showBarInfo', // 点击左侧菜单栏显示对应信息
             'click #info-btn': 'showTip', // 显示信息提示框
@@ -61,8 +69,6 @@ define(['jquery-plugin', 'underscore', 'backbone'], function(require, exports, m
         $old_el: null,
         $old_bar: null,
         $tip_btn: null,
-        $lghtml: $('#login-html'), // 保存登陆页面的html代码
-        $mainhtml: $('#main-html'),
         $content: null,
         type: null, // 视图类型
         bar: null, // 视图子类型
@@ -70,51 +76,75 @@ define(['jquery-plugin', 'underscore', 'backbone'], function(require, exports, m
             Backbone.View.apply(this, arguments);
             this.type = type;
             this.bar = bar;
-            this.initView();
+            this.render();
         },
         initialize: function () {
             _.bindAll(this, 'render');
             this.$old_el = $('#left-nav .l-menu').first().next();
             this.$tip_btn = $('#info-btn');
             this.$content = $('#content');
-            console.log('进入webapp');
-            console.log(this.type , this.bar);
-//            this.$el.find('#left-nav>.l-menu[data-type="hkmanage"]').click();
-            console.log(this.$el.find('#left-nav>.l-menu[data-type="hkmanage"]'));
+            console.log('初始化webapp');
         },
         initView: function () {
             var that = this;
             setTimeout(function () { // 待视图对象初始化后，恢复到刷新前状态
-                var $that_type = that.$el.find('#left-nav>.l-menu[data-type="' + that.type + '"]');
-                that.$old_bar = $that_type.next().find('li[data-bar="' + that.bar + '"]');
-                $that_type.click(); // 模拟点击事件，展开左侧详细菜单项
-                that.$old_bar.addClass('l-active');  // 选中菜单项
+                that.showState(that.type, that.bar);
             }, 100);
         },
         render: function () {
             // 执行渲染
+            this.initView();
+        },
+        closeType: function ($type) {
+            $type.replaceClass('t-close', 't-open');
+            $type.prev().find('span.t-rotate').replaceClass('t-rotate-close', 't-rotate-open');
+            return $type;
+        },
+        openType: function ($type) {
+            $type.replaceClass('t-open', 't-close');
+            $type.prev().find('span.t-rotate').replaceClass('t-rotate-open', 't-rotate-close');
+            return $type;
+        },
+        activeBar: function ($bar, active) {
+            if(active){
+                $bar.addClass('l-active');
+            }
+            else{
+                $bar.removeClass('l-active');
+            }
+            return $bar;
+        },
+        showState: function (type, bar) {
+            var $that_type = this.$el.find('#left-nav>.l-menu[data-type="' + type + '"]');
+            var $that_bar = $that_type.next().find('li[data-bar="' + bar + '"]');
+            this.$old_bar = $that_bar;
+            $that_bar.click();
+            this.$old_el = this.openType($that_type.next());
+        },
+        showStateData: function (e) {
+            var $ele = $(e.currentTarget),
+                type = $ele.parent().attr('data-type'),
+                bar = $ele.attr('data-bar');
+            approuter.navigate('main/' + type + '/' + bar, {trigger: false});
+            this.closeType(this.$old_el);
+            this.activeBar(this.$old_bar, false);
+            this.showState(type, bar);
         },
         togSlide: function (e) { // 滑动切换
-            var $temp_el = $(e.currentTarget);
-            var ul = $temp_el.next(), // 获取ul元素
-                span = $temp_el.find('span.t-rotate');
+            var $temp_el = $(e.currentTarget),
+                $ul = $temp_el.next(); // 获取ul元素
             this.type = $temp_el.attr('data-type'); // 获取用户点击的菜单项type
-            if (this.$old_el[0] === ul[0]) {
-                if (ul.hasClass('t-close')) {
-                    ul.replaceClass('t-open', 't-close');
-                    span.replaceClass('t-rotate-open', 't-rotate-close');
+            if (this.$old_el[0] === $ul[0]) {
+                if ($ul.hasClass('t-close')) {
+                    this.openType($ul);
                 }
                 else {
-                    ul.replaceClass('t-close', 't-open');
-                    span.replaceClass('t-rotate-close', 't-rotate-open');
+                    this.closeType($ul);
                 }
             }
             else {
-                this.$old_el.replaceClass('t-close', 't-open');
-                this.$old_el.prev().find('span.t-rotate').replaceClass('t-rotate-close', 't-rotate-open');
-                ul.replaceClass('t-open', 't-close');
-                span.replaceClass('t-rotate-open', 't-rotate-close');
-                this.$old_el = ul;
+                this.closeType(this.$old_el);
+                this.$old_el = this.openType($ul);
             }
         },
         docEvent: function () {
@@ -126,63 +156,64 @@ define(['jquery-plugin', 'underscore', 'backbone'], function(require, exports, m
             this.$tip_btn.next().show();
         },
         showInfoTip: function (e) {
-            e.preventDefault();
             e.stopPropagation();
-            console.log($(e.currentTarget).attr('data-type'))
+            this.type = 'hkmanage';
+            this.bar = 'hkdynamic';
+            window.approuter.navigate('main/hkmanage/hkdynamic', {trigger: false});
+            console.log($(e.currentTarget).attr('data-type'));
+            this.closeType(this.$old_el);
+            this.activeBar(this.$old_bar, false);
+            this.showState(this.type, this.bar);
         },
         showBarInfo: function (e) {
-            var $curli = $(e.currentTarget);
+            console.log(e.currentTarget);
+            var that = this,
+                $curli = $(e.currentTarget);
             this.bar = $curli.attr('data-bar');
             window.approuter.navigate('main/' + this.type + '/' + this.bar, {trigger: false});
-            this.$old_bar.removeClass('l-active');
-            $curli.addClass('l-active');
+            this.activeBar(this.$old_bar, false);
+            this.activeBar($curli, true);
             this.$old_bar = $curli;
-
             if(this.type == 'man' && this.bar == 'info'){
 //                var infoview = new InfoView({model: InfoModel});
                 var infomodel = new InfoModel();
 
                 var infoview = new InfoView({model: infomodel});
                 infomodel.sync('read', infoview, {
-                    url: 'http://localhost:8080/mvnhk/student/studentDetail',
-                    data: {sid: 1},
+                    url: servicepath + 'user/info',
+                    data: null,
                     dataType: 'json',
                     success: function (data) {
                         console.log('数据加载成功！', data);
+                        infomodel.set({
+                            name: data[1].name,
+                            'sex': data[1].sex,
+                            sn: data[1].teacherNo || data[1].studentNo,
+                            college: data[1].hwCollege.collegeName,
+                            major: data[1].hwMajor.name
+                        });
+                        that.$content.html(infoview.el);
                     },
                     error: function (o, e) {
                         console.log(e);
                     }
                 });
-
-                infomodel.set({
-                    name: '陈培峰',
-                    sn: '20112100187',
-                    college: '计算机学院',
-                    major: '网络工程'
-                });
-                console.log(infoview.el)
-                console.log(this.$content)
-                this.$content.html(infoview.el)
             }
         },
         exitApp: function () {
-            console.log('退出');
-            var that = this;
-            this.$el.prev().html(this.$lghtml.html());
-            setTimeout(function () {
-                //that.$el.prev().children().replaceClass('t-login-open', 't-login-close');
-            },100);
-            //this.$el.prev().children().replaceClass('t-login-open', 't-login-close');
+            approuter.navigate('login', {trigger: true});
         }
     });
 
     module.exports = {
         appView: function (type, bar) {
             console.log('创建webapp对象');
-            console.log(type, bar);
-            var a = new AppView(type, bar);
-            console.log(a);
+            if(window.appview){
+                appview.constructor(type,bar); // 传入初始参数，改变原有对象
+            }
+            else{
+                window.appview = new AppView(type, bar); // 新建view对象
+            }
         }
     }
 
